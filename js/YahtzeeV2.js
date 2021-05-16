3011a60e5b9efee09e7e4c5d9d71baedc1d024c0
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-16 09:51:10 -0500

    added scoring for top section

--- js/YahtzeeV2.js
+++ js/YahtzeeV2.js
@@ -1,5 +1,6 @@
 'use strict';
 // Build card for use with additional players later
+
 var html = "<table class='container m-0 p-0'>";
     html += "<tr>";
     html += `<th>UPPER SECTION</th>`;
@@ -14,60 +15,60 @@ var html = "<table class='container m-0 p-0'>";
     html += `<tr>`;
     html += `<td>Aces</td>`;
     html += `<td>Count and add only Aces</td>`;
-    html += `<td id="g1Ones"></td>`;
-    html += `<td id="g2Ones"></td>`;
-    html += `<td id="g3Ones"></td>`;
-    html += `<td id="g4Ones"></td>`;
-    html += `<td id="g5Ones"></td>`;
-    html += `<td id="g6Ones"></td>`;
+    html += `<td id="g1Ones" value='1'></td>`;
+    html += `<td id="g2Ones" value='1'></td>`;
+    html += `<td id="g3Ones" value='1'></td>`;
+    html += `<td id="g4Ones" value='1'></td>`;
+    html += `<td id="g5Ones" value='1'></td>`;
+    html += `<td id="g6Ones" value='1'></td>`;
     html += `</tr>`;
     html += `<tr>`;
     html += `<td>Twos`
     html += `<td>Count and add only Twos</td>`;
-    html += `<td id="g1Twos"></td>`;
-    html += `<td id="g2Twos"></td>`;
-    html += `<td id="g3Twos"></td>`;
-    html += `<td id="g4Twos"></td>`;
-    html += `<td id="g5Twos"></td>`;
-    html += `<td id="g6Twos"></td>`;
+    html += `<td id="g1Twos" value="2"></td>`;
+    html += `<td id="g2Twos" value="2"></td>`;
+    html += `<td id="g3Twos" value="2"></td>`;
+    html += `<td id="g4Twos" value="2"></td>`;
+    html += `<td id="g5Twos" value="2"></td>`;
+    html += `<td id="g6Twos" value="2"></td>`;
     html += `</tr>`;
     html += `<tr>`;
     html += `<td>Threes</td>`;
     html += `<td>Count and add only Threes</td>`;
-    html += `<td id="g1Threes"></td>`;
-    html += `<td id="g2Threes"></td>`;
-    html += `<td id="g3Threes"></td>`;
-    html += `<td id="g4Threes"></td>`;
-    html += `<td id="g5Threes"></td>`;
-    html += `<td id="g6Threes"></td>`;
+    html += `<td id="g1Threes" value="3"></td>`;
+    html += `<td id="g2Threes" value="3"></td>`;
+    html += `<td id="g3Threes" value="3"></td>`;
+    html += `<td id="g4Threes" value="3"></td>`;
+    html += `<td id="g5Threes" value="3"></td>`;
+    html += `<td id="g6Threes" value="3"></td>`;
     html += `</tr>`;
     html += `<tr>`;
     html += `<td>Fours</td>`;
     html += `<td>Count and add only Fours</td>`;
-    html += `<td id="g1Fours"></td>`;
-    html += `<td id="g2Fours"></td>`;
-    html += `<td id="g3Fours"></td>`;
-    html += `<td id="g4Fours"></td>`;
-    html += `<td id="g5Fours"></td>`;
-    html += `<td id="g6Fours"></td>`;
+    html += `<td id="g1Fours" value="4"></td>`;
+    html += `<td id="g2Fours" value="4"></td>`;
+    html += `<td id="g3Fours" value="4"></td>`;
+    html += `<td id="g4Fours" value="4"></td>`;
+    html += `<td id="g5Fours" value="4"></td>`;
+    html += `<td id="g6Fours" value="4"></td>`;
     html += `</tr>`;
     html += `<td>Fives</td>`;
     html += `<td>Count and add only Fives</td>`;
-    html += `<td id="g1Fives"></td>`;
-    html += `<td id="g2Fives"></td>`;
-    html += `<td id="g3Fives"></td>`;
-    html += `<td id="g4Fives"></td>`;
-    html += `<td id="g5Fives"></td>`;
-    html += `<td id="g6Fives"></td>`;
+    html += `<td id="g1Fives" value="5"></td>`;
+    html += `<td id="g2Fives" value="5"></td>`;
+    html += `<td id="g3Fives" value="5"></td>`;
+    html += `<td id="g4Fives" value="5"></td>`;
+    html += `<td id="g5Fives" value="5"></td>`;
+    html += `<td id="g6Fives" value="5"></td>`;
     html += `</tr>`;
     html += `<td>Sixes</td>`;
     html += `<td>Count and add only Sixes</td>`;
-    html += `<td id="g1Sixes"></td>`;
-    html += `<td id="g2Sixes"></td>`;
-    html += `<td id="g3Sixes"></td>`;
-    html += `<td id="g4Sixes"></td>`;
-    html += `<td id="g5Sixes"></td>`;
-    html += `<td id="g6Sixes"></td>`;
+    html += `<td id="g1Sixes" value="6"></td>`;
+    html += `<td id="g2Sixes" value="6"></td>`;
+    html += `<td id="g3Sixes" value="6"></td>`;
+    html += `<td id="g4Sixes" value="6"></td>`;
+    html += `<td id="g5Sixes" value="6"></td>`;
+    html += `<td id="g6Sixes" value="6"></td>`;
     html += `</tr>`;
     html += `<td>Total SCORE</td>`;
     html += `<td>---></td>`;
@@ -217,9 +218,31 @@ var html = "<table class='container m-0 p-0'>";
 
     $('#html').html(html);
 
-    // Randomizer for dice
+    // Dice image object
 
+var diceImages = [
+    {die: "img/dice.jpg"},
+    {die: "img/dice2.jpg"},
+    {die: "img/dice3.jpg"},
+    {die: "img/dice4.jpg"},
+    {die: "img/dice5.jpg"},
+    {die: "img/dice6.jpg"}
+];
 
+var games = [
+    {game1:{
+        g1Ones: "dice[i] === 1",
+        g1Twos: "dice[i] === 2",
+        g1Threes: "dice[i] === 3",
+        g1Fours: "dice[i] === 4",
+        g1Fives: "dice[i] === 5",
+        g1Sixes: "dice[i] === 6",
+}}
+]
+
+    // Start round
+
+var rollCount = 0;
 
     $(document).ready(function (){
     var startGame = confirm("Are you ready to begin?");
@@ -228,7 +251,7 @@ var html = "<table class='container m-0 p-0'>";
         }
     });
 
-
+// First roll
 
 var dice = [];
 
@@ -236,5 +259,128 @@ function firstRoll (){
     for(var i = 0; i < 5; i++) {
         dice.push(Math.floor(Math.random() * (6-0) + 1));
     }
+    matchDie();
+    rollCount++
+    $('#rollButton').prop('disabled', true);
 }
-console.log(dice);
+
+function matchDie (){
+    for(var i = 0; i < dice.length; i++){
+        $("#slot" + (i + 1)).attr("src", diceImages[(dice[i]-1)].die);
+    }
+}
+var selected = [];
+function getSelected() {
+    for (var i = 1; i < 6; i++) {
+        selected.push($("#die" + i + ":checked").val());
+    }
+    translate(selected);
+}
+
+var keptDice = [];
+var diceToReroll = [];
+function translate(x) {
+    for (var i = 0; i < x.length; i++) {
+        if (x[i] === "on") {
+            keptDice.push(dice[i]);
+        }
+    }
+    keptDice.sort();
+    diceToReroll.push(5 - (keptDice.length));
+    $('#rollButton').prop('disabled', false);
+}
+function userPress(){
+    reroll(diceToReroll);
+}
+var newDie = [];
+function reroll(x) {
+    for (var i = 0; i < x; i++) {
+        newDie.push(Math.floor(Math.random() * (6 - 0) + 1));
+    }
+    rollCount++
+    $('#rollButton').prop('disabled', true);
+    swap();
+}
+
+function swap(){
+    for(var i = 0; i < keptDice.length; i++){
+        $("#slot"+[i + 1]).attr('src', diceImages[(keptDice[i]-1)].die);
+        $("#die"+ [i + 1]).prop('checked', false);
+    }
+    console.log(i);
+    for(var j = (keptDice.length); j < 5; j++) {
+        console.log(newDie);
+        $("#slot" + [j + 1]).attr('src', diceImages[(newDie[(j-keptDice.length)] - 1)].die);
+        $("#die" + [j + 1]).prop('checked', false);
+        console.log(j);
+    }
+    if(rollCount === 3) {
+        $('#rollButton').prop('disabled', true);
+        $('#lock').prop('disabled', true);
+        alert("Please choose where you would like to score your dice.")
+    }
+    $('form').children().prop('checked', false);
+    dice = keptDice.concat(newDie);
+    console.log(dice);
+    keptDice.length = [];
+    selected.length = [];
+    diceToReroll.length = [];
+    newDie.length = [];
+}
+
+$('td').click(function (){
+    var check = $(this).html();
+    var secondCheck = $(this).attr('id')
+    if(check !== "" || secondCheck === "g13K" || secondCheck === "g14K" || secondCheck === 'g1Fh' || secondCheck === "g1Ss" || secondCheck === 'g1Ls' || secondCheck === 'g1Y' || secondCheck === 'g1C') {
+        return false;
+    }else if(check === ""){
+        $('#postRoll').prop('disabled', false);
+        var count = 0;
+        var rule = $(this).attr('id');
+        for(var i = 0; i < dice.length; i++){
+            if((parseInt($(this).attr('value'))) === dice[i]){
+                count++;
+                console.log(count);
+            }
+            var convert = "#" + rule;
+            $('#postRoll').click(function (){
+                var please = document.getElementById(rule).innerHTML = count * (parseInt(document.getElementById(rule).getAttribute('value')));
+                dice = [];
+                rollCount = 0;
+                keptDice = [];
+                selected = [];
+                diceToReroll =[];
+                newDie = [];
+                $('#postRoll').prop('disabled', true);
+                $('#lock').prop('disabled', false);
+                firstRoll();
+                addTop();
+            });
+
+        }
+    }
+});
+var topArray = ['g1Ones', 'g1Twos', 'g1Threes', 'g1Fours', 'g1Fives', 'g1Sixes'];
+var sum = []
+function addTop() {
+    var nums = [];
+    ;
+    for (var i = 0; i < topArray.length; i++) {
+        if (document.getElementById(topArray[i]).innerHTML === '' || document.getElementById(topArray[i]).innerHTML === null) {
+            nums.push(0);
+        } else {
+            nums.push(parseInt(document.getElementById(topArray[i]).innerHTML));
+        }
+        console.log(nums);
+        sum = nums.reduce((a,b) => a+b);
+        $('#g1Ts').html(sum);
+        upperTsWithBonus();
+    }
+}
+function upperTsWithBonus(){
+    if(sum >= 63){
+        sum = sum + 35;
+        $('#g1Bs').html(35);
+    }
+    $('#g1TUS').html(sum);
+}
