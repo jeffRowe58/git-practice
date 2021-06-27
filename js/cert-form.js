c2dbc0c8a9a5653bede292e0a963e575fedc82ae
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-06-26 21:15:08 -0500

    survey form

---
+++ js/cert-form.js
@@ -0,0 +1,18 @@
+function show() {
+    document.getElementById('other').style.display = 'block';
+    document.getElementById('other-label').style.display = 'block';
+}
+
+function show2(){
+    document.getElementById('checkbox-other-label').style.display = 'block';
+    document.getElementById('other-checkbox').style.display = 'block';
+}
+function hide() {
+    document.getElementById('other').style.display = 'none';
+    document.getElementById('other-label').style.display = 'none';
+ }
+
+ function hide2(){
+     document.getElementById('checkbox-other-label').style.display = 'none';
+     document.getElementById('other-checkbox').style.display = 'none';
+ }
