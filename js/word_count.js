bf458e48ffe4e781a5e693b68624038206b3daa7
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-09-10 14:00:47 -0500

    word count better

--- js/word_count.js
+++ js/word_count.js
@@ -1,32 +1,27 @@
 var string = "";
 var array = [];
 var results = [];
-$("#sendIt").click(function (){
-    string = $("#words").val();
+var wordArray = [];
+
+$('#sendIt').click(function (){
+    string = $('#input').val();
     string = string.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
-    console.log(string);
     array = string.split(" ");
-    results = getWordCntRd(array);
+    wordArray = array.filter(word => word.length >= 4);
+    results = getWordCntRd(wordArray);
     tableGenerator();
 })
 
-
-
-
-
-
 function getWordCntRd() {
-    return array.reduce((prev, nxt) => {
+    return wordArray.reduce((prev, nxt) => {
         prev[nxt] = (prev[nxt] + 1) || 1;
         return prev;
     }, {});
 }
 
-
-
 function tableGenerator(){
 Object.keys(results).forEach((key) => {
-    $("#words").append("<tr><td>" + key + "</td><td>" + results[key] + "</td></tr>");
+        $("#words").append("<tr><td>" + key + "</td><td>" + results[key] + "</td></tr>");
 })}
 
 
