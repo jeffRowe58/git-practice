179361836ec4277e3a3c0bd798f1d2275befa9fc
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-09-12 12:21:59 -0500

    formatting

--- js/thesaurusAPI.js
+++ js/thesaurusAPI.js
@@ -2,40 +2,45 @@ var input = "";
 var array = [];
 var results = [];
 var testCorrect = [];
-var res = $("#userInput").keyup(function(e){
+var res = $("#userInput").keyup(function (e) {
     var pressedKey = e.which;
-    if(pressedKey === 32){
-    input = $("#userInput").val();
-    input = input.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
-    array = input.split(" ");
-    testCorrect = array.filter( word => (word.length > 3));
-    results = getWordCntRd(testCorrect);
+    if (pressedKey === 32) {
+        input = $("#userInput").val();
+        input = input.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+        array = input.split(" ");
+        testCorrect = array.filter(word => (word.length > 3));
+        results = getWordCntRd(testCorrect);
 
-   Object.keys(results).forEach((key) => {
-       if(results[key] > 2){
-           console.log(key + " " + results[key]);
-           $('#postResults').append("<p>" + key + " " + results[key] + "</p>")
-       }
-   })
-    // $.ajax({
-    //     method: "GET",
-    //     $curl: "https://projects.propublica.org/nonprofits/api/v2/organizations/" + input + ".json",
-    //     success: function(data){
-    //             output = "";
-    //             for (dataIn in data.response) {
-    //                 console.log(dataIn);
-    //                 output = dataIn;
-    //                 console.log(output);
-    //                 $("#div1").html(output);
-    //                 input = "";
-    //             }
-    //     },
-    //         error: function(xhr, status, error){
-    //         $("#div1").html("An error occured: " + status + " " + error);
-    //     }
-    // });
+        Object.keys(results).forEach((key, index) => {
+            if (results[key] > 2) {
+                console.log(key + " " + results[key]);
+                $('#postResults').html(key + " " + results[key] + " " + index);
 
-}})
+                $.ajax({
+                    method: "GET",
+                    url: `https://thesaurus.altervista.org/thesaurus/v1?word=${key} &language=en_US&output=json&key=${THESAURUS_API_TOKEN}`,
+                    success: function (data) {
+                        output = "";
+                        outputArray = [];
+                        for (dataIn in data.response) {
+                            output = data.response[dataIn].list.synonyms;
+                            outputArray = output.split("|")
+                            outputArray.forEach((e) => {
+                                $("#div1").append("<p>" + e + "</p>");
+                            })
+                            outputArray = [];
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        $("#div1").html("An error occured: " + status + " " + error);
+                    }
+                })
+            }
+        })
+
+
+    }
+})
 
 function getWordCntRd() {
     return testCorrect.reduce((prev, nxt) => {
